d2dc0777261d4a2d4edcf46dac43a40c27493b82
yirankiller <yirankiller@gmail.com>
2012-07-23 09:37:16 -0400

    init ikiller

---
+++ ikiller/yirankiller-util/yirankiller-util.jar/src/main/java/com/ikiller/yirankiller/util/string/FormatUtil.java
@@ -0,0 +1,11 @@
+package com.ikiller.yirankiller.util.string;
+
+/**
+ * User: easliu
+ * Date: 7/20/12
+ * Version: 1.0
+ */
+public interface FormatUtil {
+
+    public String formatDate(String date);
+}
